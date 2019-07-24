deadf7ef316d2e639fbab010fe8a7e5cc1f046be
Thomas LE ROUX <thomas@leroux.io>
2019-07-24 23:18:08 +0200

    feat(appender): Add FileAppender with rolling system

--- appender.go
+++ appender.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strconv"
 	"sync"
 
 	"github.com/pkg/errors"
@@ -26,6 +28,8 @@ type Appender interface {
 	Name() string
 	// Write receives a log entry.
 	Write(entry *Entry)
+	// Close recycles underlying resources of appender.
+	Close() error
 }
 
 // IsAppenderNameValid verify that a Appender name has a valid format.
@@ -34,24 +38,27 @@ var IsAppenderNameValid = regexp.MustCompile(`^[a-z]+[a-z._0-9-]+[a-z0-9]+$`).Ma
 // NewAppender creates a new Appender from given configuration.
 // To register a custom appender, please use soba.RegisterAppenders() function.
 func NewAppender(name string, conf ConfigAppender) (Appender, error) {
+	err := validateAppenderConfig(name, conf)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot create appender for %s", name)
+	}
+
 	switch conf.Type {
 	case ConsoleAppenderType:
-		appender := &ConsoleAppender{
-			name: name,
-			out:  os.Stdout,
-		}
-
+		appender := NewConsoleAppender(name, os.Stdout)
 		return appender, nil
 
 	case FileAppenderType:
-		appender := &FileAppender{
-			name: name,
+		appender, err := NewFileAppender(name, conf.Path, conf.Backup, conf.MaxBytes)
+		if err != nil {
+			return nil, errors.Wrapf(err, "cannot create file appender for %s", name)
 		}
 
 		return appender, nil
 
 	default:
-		return nil, errors.Errorf("unknown appender type: %s", conf.Type)
+		// Should be handled by validateAppenderConfig function.
+		return nil, errors.Errorf("unknown appender type for %s: %s", name, conf.Type)
 	}
 }
 
@@ -62,9 +69,22 @@ type ConsoleAppender struct {
 	out   io.Writer
 }
 
+// NewConsoleAppender creates a new ConsoleAppender instance.
+func NewConsoleAppender(name string, out io.Writer) *ConsoleAppender {
+	return &ConsoleAppender{
+		name: name,
+		out:  out,
+	}
+}
+
 // Name returns appender name.
 func (appender *ConsoleAppender) Name() string {
 	return appender.name
+}
+
+// Close recycles underlying resources of appender.
+func (appender *ConsoleAppender) Close() error {
+	return nil
 }
 
 // Write receives a log entry.
@@ -79,16 +99,36 @@ func (appender *ConsoleAppender) Write(entry *Entry) {
 
 	_, err := appender.out.Write(buffer)
 	if err != nil {
-		// We choose to ignore the error if we cannot log it on stderr.
-		_, _ = fmt.Fprintln(os.Stderr, err.Error())
+		onAppenderWriteError(err)
 	}
 }
 
-// TODO (novln): Add a rolling system to FileAppender
-
 // FileAppender is an appender that uses a file to write log entry.
 type FileAppender struct {
-	name string
+	mutex    sync.Mutex
+	name     string
+	path     string
+	file     *os.File
+	size     int64
+	backup   bool
+	maxBytes int64
+}
+
+// NewFileAppender creates a new FileAppender instance.
+func NewFileAppender(name string, path string, backup bool, maxBytes int64) (*FileAppender, error) {
+	appender := &FileAppender{
+		name:     name,
+		path:     path,
+		backup:   backup,
+		maxBytes: maxBytes,
+	}
+
+	err := appender.openNew()
+	if err != nil {
+		return nil, errors.Wrapf(err, `cannot create file "%s" for appender`, path)
+	}
+
+	return appender, nil
 }
 
 // Name returns appender name.
@@ -96,8 +136,163 @@ func (appender *FileAppender) Name() string {
 	return appender.name
 }
 
-// Write receives a log entry.
+// Close recycles underlying resources of appender.
+func (appender *FileAppender) Close() error {
+	appender.mutex.Lock()
+	defer appender.mutex.Unlock()
+
+	return appender.close()
+}
+
+// Write receives a log entry and writes it on a file.
 func (appender *FileAppender) Write(entry *Entry) {
-	// TODO
-	fmt.Println(entry)
+	encoder := json.NewEncoder()
+	defer encoder.Close()
+
+	buffer := WriteEntry(entry, encoder)
+
+	appender.mutex.Lock()
+	defer appender.mutex.Unlock()
+
+	err := appender.rotate(len(buffer))
+	if err != nil {
+		onAppenderWriteError(err)
+		return
+	}
+
+	n, err := appender.file.Write(buffer)
+	if err != nil {
+		onAppenderWriteError(err)
+		return
+	}
+
+	appender.size += int64(n)
+}
+
+// openNew opens a new appender file to write log entry.
+func (appender *FileAppender) openNew() error {
+	if appender.file != nil {
+		return nil
+	}
+
+	directory := filepath.Dir(appender.path)
+	err := os.MkdirAll(directory, 0750)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	file, err := os.OpenFile(appender.path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	fstat, err := file.Stat()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	appender.file = file
+	appender.size = fstat.Size()
+
+	return nil
+}
+
+// close closes the underlying file.
+func (appender *FileAppender) close() error {
+	if appender.file == nil {
+		return nil
+	}
+
+	file := appender.file
+	appender.file = nil
+	appender.size = 0
+
+	err := file.Close()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+// rotate analyzes if a file rotation is required, and executes it when needed.
+func (appender *FileAppender) rotate(toWrite int) error {
+	if appender.maxBytes == 0 {
+		return nil
+	}
+
+	finalSize := int64(toWrite) + appender.size
+	if finalSize < appender.maxBytes {
+		return nil
+	}
+
+	err := appender.close()
+	if err != nil {
+		return err
+	}
+
+	err = appender.doRotate()
+	if err != nil {
+		return err
+	}
+
+	err = appender.openNew()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// doRotate executes the file rotation, using backup or rename strategy.
+func (appender *FileAppender) doRotate() error {
+	if appender.backup {
+		return appender.rotateWithBackup()
+	}
+	return appender.rotateWithRename()
+}
+
+// rotateWithRename renames the current file by suffixing it with "-".
+// If a file with the suffix already exists, it will be replaced.
+func (appender *FileAppender) rotateWithRename() error {
+	backup := fmt.Sprint(appender.path, "-")
+
+	err := os.Rename(appender.path, backup)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+// rotateWithBackup renames the current file by suffixing it with a backup number.
+//
+// Let's say with we the following files in the directory:
+//   - app.log
+//   - app.log.1
+//   - app.log.2
+//   - app.log.3
+//
+// The current file (app.log) will be rename to "app.log.4".
+func (appender *FileAppender) rotateWithBackup() error {
+	pattern := fmt.Sprint(appender.path, ".*")
+	list, err := filepath.Glob(pattern)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	id := len(list) + 1
+	backup := fmt.Sprint(appender.path, ".", strconv.FormatInt(int64(id), 10))
+
+	err = os.Rename(appender.path, backup)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	return nil
+}
+
+func onAppenderWriteError(err error) {
+	// We choose to ignore the error if we cannot log it on stderr.
+	_, _ = fmt.Fprintln(os.Stderr, err.Error())
 }
--- appender_test.go
+++ appender_test.go
@@ -1,7 +1,11 @@
 package soba_test
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	"testing"
 	"time"
 
@@ -18,6 +22,10 @@ type TestAppender struct {
 
 func (appender *TestAppender) Name() string {
 	return appender.name
+}
+
+func (TestAppender) Close() error {
+	return nil
 }
 
 func (appender *TestAppender) Write(entry *soba.Entry) {
@@ -63,6 +71,13 @@ func NewTestAppender(name string) *TestAppender {
 
 // Ensure TestAppender implements Appender interface at compile time.
 var _ soba.Appender = &TestAppender{}
+
+func CloseAppender(t *testing.T, appender soba.Appender) {
+	err := appender.Close()
+	if err != nil {
+		t.Fatalf("Unexpected error: %+v", err)
+	}
+}
 
 // Test appender name format.
 func TestAppender_IsNameValid(t *testing.T) {
@@ -167,6 +182,7 @@ func TestAppender_IsNameValid(t *testing.T) {
 // nolint: gocyclo
 func TestAppender_Write(t *testing.T) {
 	appender := NewTestAppender("foobar")
+	defer CloseAppender(t, appender)
 
 	{
 		before := time.Now()
@@ -259,10 +275,421 @@ func TestAppender_Write(t *testing.T) {
 
 // Test appender name definition for other tests.
 func TestAppender_Name(t *testing.T) {
-	appender := NewTestAppender("foobar")
 	expected := "foobar"
+	appender := NewTestAppender(expected)
+	defer CloseAppender(t, appender)
 
 	if appender.Name() != expected {
 		t.Fatalf("Unexpected appender name: %s should be %s", appender.Name(), expected)
 	}
+}
+
+// Test console appender constructor with invalid type.
+func TestAppender_InvalidNew(t *testing.T) {
+	name := "invalid"
+	appender, err := soba.NewAppender(name, soba.ConfigAppender{
+		Type: "logstash",
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid type)`, name)
+	}
+}
+
+// Test console appender constructor.
+func TestAppender_ConsoleNew(t *testing.T) {
+	name := "console1"
+	appender, err := soba.NewAppender(name, soba.ConfigAppender{
+		Type: soba.ConsoleAppenderType,
+		Path: "/var/log/output.log",
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid path)`, name)
+	}
+
+	name = "console2"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type:   soba.ConsoleAppenderType,
+		Backup: true,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid backup)`, name)
+	}
+
+	name = "console3"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type:     soba.ConsoleAppenderType,
+		MaxBytes: 60,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid max bytes)`, name)
+	}
+
+	name = "Console$"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type: soba.ConsoleAppenderType,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid name)`, name)
+	}
+
+	name = "console4"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type: soba.ConsoleAppenderType,
+	})
+	if err != nil {
+		t.Fatalf(`Unexpected error for appender "%s": %+v`, name, err)
+	}
+	if appender.Name() != name {
+		t.Fatalf("Unexpected appender name: %s should be %s", appender.Name(), name)
+	}
+	CloseAppender(t, appender)
+}
+
+// Test console appender write behavior.
+func TestAppender_ConsoleWrite(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	name := "console"
+
+	appender := soba.NewConsoleAppender(name, buffer)
+	defer CloseAppender(t, appender)
+
+	entry1 := soba.NewEntry("foobar.module.asm", soba.WarnLevel, "Invalid opcode", []soba.Field{
+		soba.Binary("opcode", []byte{0x67}),
+		soba.String("module", "bootloader"),
+	})
+	defer entry1.Flush()
+
+	entry2 := soba.NewEntry("foobar.module.asm", soba.DebugLevel, "Jump stack", []soba.Field{
+		soba.Uint64("from", 0x23456F34),
+		soba.Uint64("to", 0x6723F4AB),
+		soba.String("module", "cryptofs"),
+	})
+	defer entry2.Flush()
+
+	appender.Write(entry1)
+	appender.Write(entry2)
+
+	output := strings.TrimSpace(buffer.String())
+	lines := strings.Split(output, "\n")
+	if len(lines) != 2 {
+		t.Fatalf("Unexpected number of entries: %d should be %d", len(lines), 2)
+	}
+
+	checkContains := func(line string, key string) {
+		if !strings.Contains(line, key) {
+			t.Fatalf("Expect '%s' to be included in: %s", key, line)
+		}
+	}
+
+	checkContains(lines[0], `"logger":"foobar.module.asm"`)
+	checkContains(lines[0], `"level":"warning"`)
+	checkContains(lines[0], `"message":"Invalid opcode"`)
+	checkContains(lines[0], `"opcode":"Zw=="`)
+	checkContains(lines[0], `"module":"bootloader"`)
+
+	checkContains(lines[1], `"logger":"foobar.module.asm"`)
+	checkContains(lines[1], `"level":"debug"`)
+	checkContains(lines[1], `"message":"Jump stack"`)
+	checkContains(lines[1], `"from":591753012`)
+	checkContains(lines[1], `"to":1730409643`)
+	checkContains(lines[1], `"module":"cryptofs"`)
+}
+
+// Test file appender constructor.
+func TestAppender_FileNew(t *testing.T) {
+	path1 := ""
+	path2 := "/output.log"
+	path3 := "testdata/logs/file.log"
+
+	name := "file1"
+	appender, err := soba.NewAppender(name, soba.ConfigAppender{
+		Type:     soba.FileAppenderType,
+		Path:     path1,
+		Backup:   false,
+		MaxBytes: 0,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid path)`, name)
+	}
+
+	name = "file2"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type:     soba.FileAppenderType,
+		Path:     path2,
+		Backup:   false,
+		MaxBytes: 0,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (cannot create file)`, name)
+	}
+
+	name = "file3"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type:     soba.FileAppenderType,
+		Path:     path3,
+		Backup:   false,
+		MaxBytes: -120,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid max bytes)`, name)
+	}
+
+	name = "File$"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type:     soba.FileAppenderType,
+		Path:     path3,
+		Backup:   false,
+		MaxBytes: 0,
+	})
+	if err == nil {
+		CloseAppender(t, appender)
+		t.Fatalf(`An error was expected for appender "%s" (invalid name)`, name)
+	}
+
+	name = "file4"
+	appender, err = soba.NewAppender(name, soba.ConfigAppender{
+		Type:     soba.FileAppenderType,
+		Path:     path3,
+		Backup:   false,
+		MaxBytes: 0,
+	})
+	if err != nil {
+		t.Fatalf(`Unexpected error for appender "%s": %+v`, name, err)
+	}
+	if appender.Name() != name {
+		t.Fatalf("Unexpected appender name: %s should be %s", appender.Name(), name)
+	}
+	CloseAppender(t, appender)
+}
+
+// Test file appender write behavior.
+// nolint: gocyclo
+func TestAppender_FileWrite(t *testing.T) {
+	name := "file"
+	path1 := "testdata/logs/output.log"
+	path2 := "testdata/logs/output.log.1"
+	path3 := "testdata/logs/output.log.2"
+	path4 := "testdata/logs/output.log-"
+
+	entry1 := soba.NewEntry("foobar.module.asm", soba.WarnLevel, "Invalid opcode", []soba.Field{
+		soba.Binary("opcode", []byte{0x67}),
+		soba.String("module", "bootloader"),
+	})
+	defer entry1.Flush()
+
+	entry2 := soba.NewEntry("foobar.module.asm", soba.DebugLevel, "Jump stack", []soba.Field{
+		soba.Uint64("from", 0x23456F34),
+		soba.Uint64("to", 0x6723F4AB),
+		soba.String("module", "cryptofs"),
+	})
+	defer entry2.Flush()
+
+	deleteFiles := func() {
+		err := os.Remove(path1)
+		if err != nil && !os.IsNotExist(err) {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+
+		err = os.Remove(path2)
+		if err != nil && !os.IsNotExist(err) {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+
+		err = os.Remove(path3)
+		if err != nil && !os.IsNotExist(err) {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+
+		err = os.Remove(path4)
+		if err != nil && !os.IsNotExist(err) {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+	}
+
+	getFileContent := func(path string) []byte {
+		buffer, err := ioutil.ReadFile(path)
+		if err != nil {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+		if len(buffer) == 0 {
+			t.Fatalf("Unexpected empty file: %s", path1)
+		}
+		return buffer
+	}
+
+	getAppender := func(path string, backup bool, maxBytes int64) soba.Appender {
+		appender, err := soba.NewFileAppender(name, path, backup, maxBytes)
+		if err != nil {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+		return appender
+	}
+
+	checkContains := func(line string, key string) {
+		if !strings.Contains(line, key) {
+			t.Fatalf("Expect '%s' to be included in: %s", key, line)
+		}
+	}
+
+	checkFileA := func(path string) {
+		buffer := getFileContent(path)
+		output := strings.TrimSpace(string(buffer))
+		lines := strings.Split(output, "\n")
+		if len(lines) != 2 {
+			t.Fatalf("Unexpected number of entries: %d should be %d", len(lines), 2)
+		}
+
+		checkContains(lines[0], `"logger":"foobar.module.asm"`)
+		checkContains(lines[0], `"level":"warning"`)
+		checkContains(lines[0], `"message":"Invalid opcode"`)
+		checkContains(lines[0], `"opcode":"Zw=="`)
+		checkContains(lines[0], `"module":"bootloader"`)
+
+		checkContains(lines[1], `"logger":"foobar.module.asm"`)
+		checkContains(lines[1], `"level":"debug"`)
+		checkContains(lines[1], `"message":"Jump stack"`)
+		checkContains(lines[1], `"from":591753012`)
+		checkContains(lines[1], `"to":1730409643`)
+		checkContains(lines[1], `"module":"cryptofs"`)
+	}
+
+	checkFileB := func(path string) {
+		buffer := getFileContent(path)
+		output := strings.TrimSpace(string(buffer))
+		lines := strings.Split(output, "\n")
+		if len(lines) != 1 {
+			t.Fatalf("Unexpected number of entries: %d should be %d", len(lines), 1)
+		}
+
+		checkContains(lines[0], `"logger":"foobar.module.asm"`)
+		checkContains(lines[0], `"level":"warning"`)
+		checkContains(lines[0], `"message":"Invalid opcode"`)
+		checkContains(lines[0], `"opcode":"Zw=="`)
+		checkContains(lines[0], `"module":"bootloader"`)
+	}
+
+	checkFileC := func(path string) {
+		buffer := getFileContent(path)
+		output := strings.TrimSpace(string(buffer))
+		lines := strings.Split(output, "\n")
+		if len(lines) != 7 {
+			t.Fatalf("Unexpected number of entries: %d should be %d", len(lines), 7)
+		}
+
+		for i, line := range lines {
+			if (i % 2) == 0 {
+				checkContains(line, `"logger":"foobar.module.asm"`)
+				checkContains(line, `"level":"warning"`)
+				checkContains(line, `"message":"Invalid opcode"`)
+				checkContains(line, `"opcode":"Zw=="`)
+				checkContains(line, `"module":"bootloader"`)
+			} else {
+				checkContains(line, `"logger":"foobar.module.asm"`)
+				checkContains(line, `"level":"debug"`)
+				checkContains(line, `"message":"Jump stack"`)
+				checkContains(line, `"from":591753012`)
+				checkContains(line, `"to":1730409643`)
+				checkContains(line, `"module":"cryptofs"`)
+			}
+		}
+	}
+
+	checkFileEmpty := func(path string) {
+		buffer, err := ioutil.ReadFile(path)
+		if err != nil && !os.IsNotExist(err) {
+			t.Fatalf("Unexpected error: %+v", err)
+		}
+		if len(buffer) != 0 {
+			t.Fatalf("An empty file was expected: %s", string(buffer))
+		}
+	}
+
+	{
+		// Test appender with a backup enabled and a limit on file size.
+		deleteFiles()
+
+		appender := getAppender(path1, true, 400)
+		defer CloseAppender(t, appender)
+
+		appender.Write(entry1)
+		appender.Write(entry2)
+
+		checkFileA(path1)
+		checkFileEmpty(path2)
+		checkFileEmpty(path3)
+		checkFileEmpty(path4)
+
+		// Write an entry that execute two file rotation.
+		appender.Write(entry1)
+		appender.Write(entry2)
+		appender.Write(entry1)
+
+		checkFileB(path1)
+		checkFileA(path2)
+		checkFileA(path3)
+		checkFileEmpty(path4)
+	}
+	{
+		// Test appender with a backup disabled and a limit on file size.
+		deleteFiles()
+
+		appender := getAppender(path1, false, 400)
+		defer CloseAppender(t, appender)
+
+		appender.Write(entry1)
+		appender.Write(entry2)
+
+		checkFileA(path1)
+		checkFileEmpty(path2)
+		checkFileEmpty(path3)
+		checkFileEmpty(path4)
+
+		// Write an entry that execute two file rotation.
+		appender.Write(entry1)
+		appender.Write(entry2)
+		appender.Write(entry1)
+
+		checkFileB(path1)
+		checkFileA(path4)
+		checkFileEmpty(path2)
+		checkFileEmpty(path3)
+	}
+	{
+		// Test appender with a backup disabled and no limit on file size.
+		deleteFiles()
+
+		appender := getAppender(path1, false, 0)
+		defer CloseAppender(t, appender)
+
+		appender.Write(entry1)
+		appender.Write(entry2)
+
+		checkFileA(path1)
+		checkFileEmpty(path2)
+		checkFileEmpty(path3)
+		checkFileEmpty(path4)
+
+		// Write an entry that execute no file rotation.
+		appender.Write(entry1)
+		appender.Write(entry2)
+		appender.Write(entry1)
+		appender.Write(entry2)
+		appender.Write(entry1)
+
+		checkFileC(path1)
+		checkFileEmpty(path2)
+		checkFileEmpty(path3)
+		checkFileEmpty(path4)
+	}
+
+	// Cleanup
+	deleteFiles()
 }
